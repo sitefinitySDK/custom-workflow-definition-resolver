ec5367d4afd8b3dca05a96b605cd62fea318c032
gyanchev <gyanchev@telerik.com>
2015-03-05 14:59:07 +0200

    Initial code.

---
+++ CustomWorkflowDefinitionResolver/CustomWorkflowDefinitionResolver.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Pages.Model;
+using Telerik.Sitefinity.Security;
+using Telerik.Sitefinity.Security.Configuration;
+using Telerik.Sitefinity.Web;
+using Telerik.Sitefinity.Workflow;
+using Telerik.Sitefinity.Workflow.Model;
+
+namespace Telerik.Sitefinity.Samples.CustomWorkflowDefinitionResolver
+{
+    public class CustomWorkflowDefinitionResolver : WorkflowDefinitionResolver
+    {
+        public CustomWorkflowDefinitionResolver()
+        {
+            this.workflowForProducts = this.CreateWorkflowForProducts();
+            this.workflowForResources = this.CreateWorkflowForResources();
+            this.workflowForDocuments = base.GetWorkflowExecutionDefinitionByTitle("Workflow for Documents");
+        }
+
+        public override IWorkflowExecutionDefinition ResolveWorkflowExecutionDefinition(IWorkflowResolutionContext context)
+        {
+            // Return pre-created workflows for "Products", "Resources" and "Documents".
+            if (this.IsPageUnder(context, "Products"))
+                return this.workflowForProducts;
+            if (this.IsPageUnder(context, "Resources"))
+                return this.workflowForResources;
+            if (this.IsPageUnder(context, "Documents") && this.workflowForDocuments != null)
+                return this.workflowForDocuments;
+
+            // The default constructor of WorkflowExecutionDefinitionProxy will instantiate
+            // a simple no-approval-needed workflow.
+            else if (this.IsPageUnder(context, "Forum"))
+                return WorkflowDefinitionProxy.DefaultWorkflow;
+
+            return base.ResolveWorkflowExecutionDefinition(context);
+        }
+
+        public override IWorkflowExecutionDefinition GetWorkflowExecutionDefinition(Guid id)
+        {
+            // Here we need to handle ids of the definitions that we created in memory.
+            // There is no need to handle the id of 'workflowForDocuments', because
+            // it is persisted in the database and the call to the base class will take
+            // care of it.
+            if (id == this.workflowForProducts.Id)
+                return this.workflowForProducts;
+            else if (id == this.workflowForResources.Id)
+                return this.workflowForResources;
+            else
+                return base.GetWorkflowExecutionDefinition(id);
+        }
+
+        // Checks whether we are under a top-level page with a certain title.
+        private bool IsPageUnder(IWorkflowResolutionContext context, string topLevelTitle)
+        {
+            if (typeof(PageNode).IsAssignableFrom(context.ContentType) &&
+                context.ContentId != Guid.Empty)
+            {
+                var siteMap = SitefinitySiteMap.GetCurrentProvider() as SiteMapBase;
+                PageSiteNode pageSiteNode = (PageSiteNode)siteMap.FindSiteMapNodeFromKey(context.ContentId.ToString());
+
+                while (pageSiteNode != null)
+                {
+                    if (pageSiteNode.Title == topLevelTitle && pageSiteNode.ParentNode == pageSiteNode.RootNode)
+                    {
+                        return true;
+                    }
+
+                    pageSiteNode = pageSiteNode.ParentNode as PageSiteNode;
+                }
+            }
+
+            return false;
+        }
+
+        private IWorkflowExecutionDefinition CreateWorkflowForProducts()
+        {
+            var backendUsersRole = Config.Get<SecurityConfig>().ApplicationRoles[SecurityConstants.AppRoles.BackendUsers];
+
+            // "Approve" is the only level in 1-step approval process.
+            // In 2-level approval the steps are "Approve" and "Publish".
+            var approvePermission = new WorkflowExecutionPermissionProxy("Approve", backendUsersRole.Id.ToString(), backendUsersRole.Name, WorkflowPrincipalType.Role);
+            return new WorkflowDefinitionProxy(
+                                                                this.workflowForProductsId,
+                                                                "1-step workflow for 'Products'",
+                                                                WorkflowType.StandardOneStep,
+                                                                true,
+                                                                new List<string> { "publishers@example.com" },
+                                                                false,
+                                                                null,
+                                                                false,
+                                                                false,
+                                                                null,
+                                                                new List<IWorkflowExecutionPermission> { approvePermission });
+        }
+
+        private IWorkflowExecutionDefinition CreateWorkflowForResources()
+        {
+            var permissions = new List<IWorkflowExecutionPermission>();
+
+            // Allow everyone in the role "Editors" to approve content.
+            var approver1 = Config.Get<SecurityConfig>().ApplicationRoles[SecurityConstants.AppRoles.Designers];
+
+            // In 2-level approval the first step is "Approve"
+            permissions.Add(new WorkflowExecutionPermissionProxy("Approve", approver1.Id.ToString(), approver1.Name, WorkflowPrincipalType.Role));
+
+            // Allow user "johnsmith" (if exists) to publish content.
+            var publisher1 = UserManager.FindUser("johnsmith");
+
+            // In 2-level approval the second step is "Publish"
+            if (publisher1 != null)
+                permissions.Add(new WorkflowExecutionPermissionProxy("Publish", publisher1.Id.ToString(), publisher1.UserName, WorkflowPrincipalType.User));
+
+            // Also allow everyone in the role "Designers" to publish content.
+            var publisher2 = Config.Get<SecurityConfig>().ApplicationRoles[SecurityConstants.AppRoles.Editors];
+            permissions.Add(new WorkflowExecutionPermissionProxy("Publish", publisher2.Id.ToString(), publisher2.Name, WorkflowPrincipalType.Role));
+
+            return new WorkflowDefinitionProxy(
+                                                                this.workflowForResourcesId,
+                                                                "2-step workflow for 'Resources'",
+                                                                WorkflowType.StandardTwoStep,
+                                                                true,
+                                                                new List<string> { "authors@example.com" },
+                                                                true,
+                                                                new List<string> { "publishers@example.com" },
+                                                                false,
+                                                                true,
+                                                                null,
+                                                                permissions);
+        }
+
+        private IWorkflowExecutionDefinition GetWorkflowExecutionDefinitionFromDb(string title)
+        {
+            return base.GetWorkflowExecutionDefinitionByTitle(title);
+        }
+
+        private readonly Guid workflowForProductsId = new Guid("D50640C8-0ECB-4CD0-8C7E-2C258235A7AD");     // Randomly generated.
+        private readonly IWorkflowExecutionDefinition workflowForProducts = null;
+
+        private readonly Guid workflowForResourcesId = new Guid("853F0A55-E3D7-4AB2-B590-296D59E175EF");    // Randomly generated.
+        private readonly IWorkflowExecutionDefinition workflowForResources = null;
+
+        private readonly IWorkflowExecutionDefinition workflowForDocuments = null;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,101 @@
-# custom-workflow-definition-resolver
+Custom workflow definitions in Sitefinity
+==========================
+Sitefinity's workflow feature allows certain content to require reviewing by a privileged user before publishing. In most cases you should be able to configure everything you need via the admin interface. However, sometimes you may want to enforce more sophisticated rules for determining the way a certain item needs to be approved. For example (but not limited to) you may want pages under certain root category to be approved by one set of approvers, while the rest of the pages to be approved by the default approvers.
+
+So, how do you inject your own code to determine the approval workflow for content items?
+With Sitefinity version 8.0, you can do it by descending from **WorkflowDefinitionResolver** and register you new class with the **ObjectFactory**.
+
+## WorkflowDefinitionResolver
+This class has 2 overridable methods:
+* **IWorkflowExecutionDefinition ResolveWorkflowExecutionDefinition(IWorkflowResolutionContext)** returns a workflow which will be used for approval of the item described by the **IWorkflowResolutionContext** parameter. These are the most common implementations:
+	1. Find an existing workflow definition in the database.
+	2. Create your own definition using **WorkflowDefinitionProxy** class.
+	3. Call base class's method for default behaviour.
+* **IWorkflowExecutionDefinition GetWorkflowExecutionDefinition(Guid)** given a Guid, returns workflow definition. If the Guid exists in the database, then you can just call the base class's implementation which will return it correctly. However if you created your own definition with code in **ResolveWorkflowExecutionDefinition**, then you need to return that definition here.
+
+## CustomWorkflowDefinitionResolver example.
+The example in this repository is a fully functional class derived from the standard **WorkflowDefinitionResolver**. The example demonstrates how you can override default workflow when a page is located under certain root pages. More specifically it handles content like this:
+
+	1. If the content is not page, then use the default workflow.
+	2. If the content is a page under a top-level page called "Products", then a custom generated 1-step workflow is returned.
+	3. If the content is a page under a top-level page called "Resources", then a custom generated 2-step workflow is returned.
+	4. If the content is a page under a top-level page called "Documents", then it finds in the database a workflow definition called "Workflow for Documents".
+	5. Otherwise uses the default workflow provided by the base class.
+
+## Prerequisites
+* Sitefinity 8.0 or above
+
+## Build and install
+Add to your Global.asax:
+
+```C#
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Initialized += this.Bootstrapper_Initialized;
+        }
+
+        void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
+        {
+            if (e.CommandName == "Bootstrapped")
+            {
+                ObjectFactory.Container.RegisterType<Telerik.Sitefinity.Workflow.IWorkflowDefinitionResolver, Telerik.Sitefinity.Samples.CustomWorkflowDefinitionResolver.CustomWorkflowDefinitionResolver>();
+            }
+        }
+    }
+```
+
+## API Overview
+### IWorkflowResolutionContext
+The **IWorkflowResolutionContext** type has several properties, which hold information about the content item which the workflow will be applied to. The following table provides more details about each property.
+
+<table>
+	<thead>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>Property</strong></td>
+			<td><strong>Description</strong></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>ContentType</strong></td>
+			<td>
+				<p>
+					.NET type of the item.
+				</p>
+			</td>
+		</tr>
+		<tr>
+			<td><strong>string</strong></td>
+			<td><strong>ContentProviderName</strong></td>
+			<td>
+				Name of item's provider.
+			</td>
+		</tr>
+		<tr>
+			<td><strong>Guid</strong></td>
+			<td><strong>ContentId</strong></td>
+			<td>
+				Item's id.
+			</td>
+		</tr>
+		<tr>
+			<td><strong>CultureInfo</strong></td>
+			<td><strong>Culture</strong></td>
+			<td>
+				Content item might be translated into multiple languages, and each translation is approved independently. This parameter tells which translation needs to be approved.
+			</td>
+		</tr>
+		<tr>
+			<td><strong>ISite</strong></td>
+			<td><strong>Site</strong></td>
+			<td>
+				The site where content was edited. In some rare cases content items might be shared between multiple sites, so approval in one site will publish it to all.
+			</td>
+		</tr>
+	</tbody>
+</table>
+
